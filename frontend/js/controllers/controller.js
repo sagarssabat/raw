5527794d208c0f1b922b669837f2e97fed193f06
sagarssabat <sagarssabat@gmail.com>
2017-04-24 19:03:32 +0530

    html structure started

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -4,10 +4,19 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         $scope.navigation = NavigationService.getNavigation();
 
         $scope.mySlides = [
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-            'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+            'img/home/slide-1.jpg',
+            'img/home/slide-1.jpg'
+        ];
+        $scope.homeSlide = [{
+                title: 'Grooming Essentials Tailored For Men',
+                img: 'img/home/slide-1.jpg',
+                btntxt: 'Explore Raw Nature'
+            },
+            {
+                title: 'Grooming Essentials Tailored For Men',
+                img: 'img/home/slide-1.jpg',
+                btntxt: 'Explore Raw Nature'
+            }
         ];
         var abc = _.times(100, function (n) {
             return n;
