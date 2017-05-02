fe9c726541f350820e55fe692efff0d6e132f9ce
sagarssabat <sagarssabat@gmail.com>
2017-05-02 19:15:33 +0530

    homepage done with remaining header and footer

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,17 +1,33 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-        name: "Home",
+        name: "Shop",
         classis: "active",
         anchor: "home",
-        subnav: [{
-            name: "Subnav1",
-            classis: "active",
-            anchor: "home"
-        }]
+        subnav: []
+        // subnav: [{
+        //     name: "Subnav1",
+        //     classis: "active",
+        //     anchor: "home"
+        // }]
     }, {
-        name: "Form",
+        name: "Gift Sets",
         classis: "active",
-        anchor: "form",
+        anchor: "gift",
+        subnav: []
+    }, {
+        name: "Rewards",
+        classis: "active",
+        anchor: "rewards",
+        subnav: []
+    }, {
+        name: "Our Story",
+        classis: "active",
+        anchor: "story",
+        subnav: []
+    }, {
+        name: "Contact Us",
+        classis: "active",
+        anchor: "contact",
         subnav: []
     }];
 
